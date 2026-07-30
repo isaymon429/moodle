13bd16f88671f678de253738a40add052fc57ddc
isaymon429 <isaymon429@gmail.com>
2026-07-30 22:29:22 +0500

    Implement navigation drawer

--- lib/widgets/nav_drawer.dart
+++ lib/widgets/nav_drawer.dart
@@ -31,7 +31,7 @@ class NavDrawer extends StatelessWidget {
                   ),
                   SizedBox(height: 10),
                   Text(
-                    'Yourname Here',
+                    'MD SIMON ISLAM',
                     style: TextStyle(
                       color: moodleWhite,
                       fontWeight: FontWeight.bold,
@@ -39,7 +39,7 @@ class NavDrawer extends StatelessWidget {
                     ),
                   ),
                   Text(
-                    'up1234567@myport.ac.uk',
+                    'up2201603@myport.ac.uk',
                     style: TextStyle(
                       color: Colors.white70,
                       fontSize: 12,
